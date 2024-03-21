9cfdfa951266029b58dbfb9fdf01b2db1072032d
henoktesfaye700 <henoktesfaye700@gmail.com>
2024-03-21 03:43:39 +0300

    format3

--- learning_project/learning_project/urls.py
+++ learning_project/learning_project/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for learning_project project.
+"""teaching_blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+import app_users
+import curriculum
 
 urlpatterns = [
+    path('', include('app_users.urls')),
+    path('curriculum/',include('curriculum.urls')),
     path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
